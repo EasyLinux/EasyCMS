7d423c8fef368694b6234129d855e31b4f49fd88
Serge NOEL <serge.noel@net6a.com>
2020-03-21 12:04:37 +0100

    Correction menu, autorise des sous niveaux

--- js/EasyCMS.js
+++ js/EasyCMS.js
@@ -7,6 +7,7 @@ $(function () {
   $.get("json/menu.json", function (datas, status) {
     gMenuItem = datas.menuItems; // Store menu for future use
     console.log(datas.menuItems);
+    //$(menu).append("<div id='navbarContent' class='collapse navbar-collapse'></div>");
     datas.menuItems.forEach(function (menuItem) {
       // Add first levels menuitems
       setItem("#menu", menuItem);
@@ -14,22 +15,28 @@ $(function () {
   });
   // Load main page  
   $("#content").load("content/main.html");
+  // Load footer
+  $("#footer").load("content/footer.html");
 
-  // Multi Level dropdowns - add from Bootstrap 4 default
-  $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function (event) {
-    event.preventDefault();
-    event.stopPropagation();
+  setTimeout(function(){
+    // Multi Level dropdowns - add from Bootstrap 4 default
+    // This must be launch after DOM is ready (wait 100ms)
+    $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function (event) {
+      event.preventDefault();
+      event.stopPropagation();
 
-    $(this).siblings().toggleClass("show");
+      $(this).siblings().toggleClass("show");
 
-    if (!$(this).next().hasClass('show')) {
-      $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-    }
-    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
-      $('.dropdown-submenu .show').removeClass("show");
+      if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+      }
+      $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+        $('.dropdown-submenu .show').removeClass("show");
+      });
+
     });
-
-  });
+  },100);
+  
 });
 
 /****************
@@ -45,6 +52,7 @@ $(function () {
  * @return void
  */
 function setItem(menu, item) {
+
   if (item.type != "menu") {
     // simple <li> line
     // <li class="nav-item"><a href="#" class="nav-link" onclick="doMenu();return false;">About</a></li>
@@ -56,7 +64,7 @@ function setItem(menu, item) {
     // <li> line plus <ul> sub-menu
     li = "<li class='nav-item dropdown'>";
     li += "<a id='menu-" + item.name + "' href='#' data-toggle='dropdown'";
-    li += " aria-haspopup='true' aria-expanded='false' class='nav-link";
+    li += " aria-haspopup='true' aria-expanded='false' class='nav-link ";
     li += "dropdown-toggle'>" + item.label + "</a>";
     li += "<ul aria-labelledby='menu-" + item.name + "' id='m-" + item.name + "' ";
     li += "class='dropdown-menu border-0 shadow'></ul></li>";
